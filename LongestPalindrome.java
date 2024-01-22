b51de419d1782b36d1447f3dbb881be4d273ba91
Chozharajan2001 <93088046+Chozharajan2001@users.noreply.github.com>
2024-01-22 15:48:42 +0530

    Create LongestPalindrome.java

---
+++ LongestPalindrome.java
@@ -0,0 +1,34 @@
+//to find the longest palindrome in the string
+class LongestPalindrome{
+    public static void main(String[] args){
+        String str = "bala";
+        palindrome(str);
+    }
+    public static void palindrome(String s){
+        int start , max_len;
+        start = 0;
+        max_len = 1;
+        for(int i = 0; i<s.length(); i++){
+            for(int j = i; j<s.length(); j++){
+                boolean flag = true;
+                
+                for(int k = 0; k<(j-i+1)/2; k++){
+                    if(s.charAt(i+k) != s.charAt(j-k)){
+                        flag = false;
+                    }
+                }
+                
+                if(flag == true && (j-i+1)>max_len){
+                    start = i;
+                    max_len = j-i+1;
+                }
+            }
+        }
+        String result = new String();
+        //To print palindrome
+        for(int i = start; i<=max_len; i++){
+            result = result+s.charAt(i);
+        }
+        System.out.println("The Longest Palindrome is: "+result+"\nlength:"+result.length());
+    }
+}
